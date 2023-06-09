cfcc1b629753377428078ceba31aeb2eab1500be
Mansilla <marcos.rehtanz@gmail.com>
2023-06-09 15:53:57 -0300

    Solution to EventList

---
+++ firebase.js
@@ -0,0 +1,67 @@
+
+// Importar las funciones necesarias de los SDK que se necesiten
+
+
+import { getFirestore, collection,addDoc,getDocs  } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+ export const separarFechaHora=(entrada)=> {
+    var partes = entrada.split(" ");
+    var dia = partes[0];
+    var mes = partes[2];
+    var año = partes[4];
+    var hora = partes[5];
+
+    return {
+        mes: mes,
+        dia: dia,
+        año: año,
+        hora: hora
+    };
+}
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyDiaTR7vehKdCH3g3qg0A955ISU6Ge3f6Y",
+    authDomain: "jovenes-teen-calendar.firebaseapp.com",
+    projectId: "jovenes-teen-calendar",
+    storageBucket: "jovenes-teen-calendar.appspot.com",
+    messagingSenderId: "296381667925",
+    appId: "1:296381667925:web:e17b9dc68c6841f42952dc"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Método para guardar datos en una colección
+// Método para guardar datos en una colección
+export const saveFecha = async (fecha, titulo, descripcion) => {
+    try {
+      const docRef = await addDoc(collection(db, "fecha"), {
+        fecha: fecha,
+        titulo: titulo,
+        descripcion: descripcion
+      });
+      console.log("Documento guardado con ID:", docRef.id);
+    } catch (error) {
+      console.error("Error al guardar el documento:", error);
+    }
+  };
+
+  export const getAllFechas = async () => {
+    try {
+      const querySnapshot = await getDocs(collection(db, "fecha"));
+      const fechas = querySnapshot.docs.map((doc) => doc.data());
+      return fechas;
+    } catch (error) {
+      console.error("Error al obtener las fechas:", error);
+      throw error;
+    }
+  };
+  
+
+
+
+
---
+++ index.js
@@ -0,0 +1,58 @@
+import { getAllFechas, saveFecha, separarFechaHora } from './firebase.js';
+var mesActual =localStorage.getItem('mesActual');
+/**function saveFechaSelect() {
+    calendar.getElement().addEventListener('change', e => {
+        var resultado = calendar.value().format('LLL');
+        localStorage.setItem("fechaSeleccionada", resultado);
+    });
+}*/
+
+const mostrarFechaMes = async (mes) => {
+    const contentFechas = document.getElementById("fechas-contenedor");
+    const querySnapshot = await getAllFechas();
+    let template = "";
+
+    querySnapshot.forEach(element => {
+        let resultado = separarFechaHora(element.fecha);
+
+        if (mes.trim().toLowerCase() === resultado.mes.trim().toLowerCase()) {
+            template += `<li class="list-group-item d-flex justify-content-between align-items-start"
+                style="background-color: #212529;">
+                <div class="ms-2 me-auto" style="color: blanchedalmond; background-color: #212529;">
+                    <div class="fw-bold">${element.fecha}</div>
+                </div>
+                <span class="badge rounded-pill" style="background-color: #FBA007;">${resultado.dia}</span>
+            </li>`;
+        }
+    });
+
+    contentFechas.innerHTML = template;
+   
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+    //saveFechaSelect();
+    let mesActual = localStorage.getItem('mesActual');
+    mostrarFechaMes(mesActual);
+    
+});
+
+const btsLeft = document.getElementById('btnEventLeft');
+const btsRight = document.getElementById('btnEventRight');
+
+btsLeft.addEventListener('click', async () => {
+    console.log(localStorage.getItem('mesActual'));
+
+    mostrarFechaMes(localStorage.getItem('mesActual'));
+});
+
+btsRight.addEventListener('click', async () => {
+    console.log(localStorage.getItem('mesActual'));
+
+    await mostrarFechaMes(localStorage.getItem('mesActual'));
+    //let mes = localStorage.getItem('mesActual');
+    //if (mes) {
+    //    console.log('Se ha cambiado el mes');
+    //    console.log(mes);
+    //}
+});
